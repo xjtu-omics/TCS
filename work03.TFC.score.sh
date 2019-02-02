f3b93f71876953fbae3a95a64a7208f53b87ab26
YituLiu <41045764+YituLiu@users.noreply.github.com>
2019-02-02 14:06:45 +0800

    Update work03.TFC.score.sh

--- work03.TFC.score.sh
+++ work03.TFC.score.sh
@@ -1,1 +1,1 @@
-perl bin/TFC_score_Calculate.v1.3.pl -highfile test/High.Test.VJ.AA.xls -distdir test/Result_AADist/ -outdir test/TFCscore.Stat -sample Test
+perl bin/TCS_score_Calculate.v1.3.pl -highfile test/High.Test.VJ.AA.xls -distdir test/Result_AADist/ -outdir test/TFCscore.Stat -sample Test
